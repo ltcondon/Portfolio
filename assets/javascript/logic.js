2888d0de28a26b838d45f9220fe93990cd1d7e8c
Liam Condon <condonliamt@gmail.com>
2019-03-17 23:04:57 -0700

    added typwriter effect

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
     $('.parallax').parallax();
 });
+
+var app = document.getElementById('typewriter-input');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('<strong>Web Developer</strong>')
+    .pauseFor(2500)
+    .deleteAll()
+    .start()
+        .typeString('<strong>Entrepreneur</strong>')
+    .pauseFor(2500)
+    .deleteAll()
+    .start()
+        .typeString('<strong>Software Engineer</strong>')
+    .pauseFor(2500)
+    .deleteAll()
+    
